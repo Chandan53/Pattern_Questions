1310a2807a568403cd47942a1f6179c662369a9c
chandan <chandankumar936672@gmail.com>
2025-07-10 23:18:35 +0530

    pattern2

---
+++ pattern2.java
@@ -0,0 +1,13 @@
+package Pattern_Questions;
+
+public class pattern2 {
+    public static void main(String args[]){
+        for(int i=1;i<=4;i++){
+            for(int j=i;j<=4;j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+    
+}
