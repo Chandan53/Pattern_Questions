a4afa4ae62812edd5b67e329c843f84ab8034ead
chandan <chandankumar936672@gmail.com>
2025-07-10 23:22:34 +0530

    pattern2

--- pattern2.java
+++ pattern2.java
@@ -1,6 +1,14 @@
 package Pattern_Questions;
 
 public class pattern2 {
+    /*
+     * inner loop count the lines
+     * outer loop count the star in each line
+     * line1 i=1 ---> 1<=4---> ****
+     * line2 i=2 --> 2<=4-->   ***
+     * line3 i=3 --> 3<=4--->  **
+     * line4 i=4 --> 4<=4-->   *
+    */
     public static void main(String args[]){
         for(int i=1;i<=4;i++){
             for(int j=i;j<=4;j++){
